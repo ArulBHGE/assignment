13b537bd761e6aa440bb59b424469740978411ea
ArulBHGE <101313097+ArulBHGE@users.noreply.github.com>
2022-03-11 14:22:18 +0530

    Add files via upload

---
+++ assigment4.1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 14:12:38 2022
+
+@author: 105042833
+"""
+import math
+x=input("enter the number to be added:")
+y=int(x)
+num=lambda y: y+25
+print(num(y))
+print("--------------------")
