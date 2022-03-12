33be38a7479a5e3d0b87be5e7007c2361978fe4b
ArulBHGE <101313097+ArulBHGE@users.noreply.github.com>
2022-03-12 06:53:51 +0530

    Add files via upload

--- assigment4.3.py
+++ assigment4.3.py
@@ -15,3 +15,17 @@ for i in range(0,n):
 print("the given list to be squared is:", lst_1)
 res = list(map(operator.mul, lst_1, lst_1))
 print("squared list is:",res)
+
+'''
+x=input("list to be squared (no space or camma):")
+y=list(x)
+z=list(map(int, x))
+print("given list to be squared",z)
+#a=input("constant to be multiplied:")
+#print(a)
+#b=list(map(int, a))
+#c=list(b)*len((z))
+#print(c)
+res = list(map(operator.mul, z, z))
+print("the list after square:"+str(res))
+'''
