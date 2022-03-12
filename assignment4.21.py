4e312fbbeef405c595ef9b00b6bff794b6bc805d
ArulBHGE <101313097+ArulBHGE@users.noreply.github.com>
2022-03-12 06:46:19 +0530

    Add files via upload

---
+++ assignment4.21.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 06:14:01 2022
+
+@author: 105042833
+"""
+import math
+import operator
+lst_1=[]
+n=int(input("enter the number of elements:"))
+for i in range(0,n):
+    ele=int(input())
+    lst_1.append(ele)
+print("the given list to be multiplied is:", lst_1)
+list_2=[]
+a=int(input("constant to be multiplied:"))
+#print(a)
+#print(type(a))
+for i in range (0,len(lst_1)):
+    list_2.append(a)
+#print(list_2)
+#print(type(list_2))
+res = list(map(operator.mul, lst_1, list_2))
+print("list after multiplication:", (res))
+
